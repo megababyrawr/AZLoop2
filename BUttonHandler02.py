2fea39c2f5dc26a147402e87a4df0623bac013dd
megababyrawr <babyrawr321@gmail.com>
2019-05-14 17:40:01 -0700

    fpga first encounter

---
+++ BUttonHandler02.py
@@ -0,0 +1,59 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from MainUI02 import Ui_MainWindow
+
+class mainProgram(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(mainProgram, self).__init__(parent)
+        self.setupUi(self)
+
+        self.pushButtonOn.clicked.connect(self.on)
+
+        self.pushButtonOff.clicked.connect(self.off)
+
+        self.pushButtonPrepDataValues.clicked.connect(self.prepdatavalues)
+
+        self.pushButtonSendCommand.clicked.connect(self.pushbuttonsendcommand)
+
+        self.pushButtonHaltCommand.clicked.connect(self.pushbuttonhaltcommand)
+
+
+    def on(self):
+
+    def off(self):
+        
+    def prepdatavalues(self):
+        self.labelPreppedTargetSpeed.setText(self.lineEditTargetSpeed.text())
+        self.labelPreppedInTime.setText(self.lineEditInTime.text())
+
+    def pushbuttonprepsettings(self):
+        self.labelPreppedCurveMode.setText(self.comboBoxCurveChoices.currentText())
+
+    # sends command and prints to textBrowser
+    def pushbuttonsendcommand(self):
+        preppedSpeed = int(self.labelPreppedTargetSpeed.text())  # NEED to make all labels 0 initially
+        preppedTime = int(self.labelPreppedInTime.text())
+        preppedSetting = self.labelPreppedCurveMode.text()
+        if not self.checkBoxSendConfirm.isChecked():
+            toBeSent = "Send Confirm is not checked."
+            self.textBrowserDisplay.setText(toBeSent)
+        if self.checkBoxSendConfirm.isChecked():
+            toBeSent = "Target speed of " + str(preppedSpeed) + " in " + str(preppedTime) + " seconds using a " + preppedSetting + " command sent."
+            self.textBrowserDisplay.setText(toBeSent)
+            self.checkBoxSendConfirm.setChecked(False)
+          #need actual send code here
+
+    def pushbuttonhaltcommand(self):
+        #sends halt command and prints to textBrowser
+        self.textBrowserDisplay.setText("Halt Command Sent")
+
+        #actual halt code
+
+
+if __name__ == "__main__":
+
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    nextGui = mainProgram()
+    nextGui.show()
+    sys.exit(app.exec_())
